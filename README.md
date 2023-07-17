87c21cfa4d8a421aeb076c8bee873ef3ab8f39d5
Tanisha Barreau <tanisha.barreau101@gmail.com>
2023-07-17 00:24:52 -0400

    added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Logo-Maker
+# Project Name - Logo-Maker
+
+# Project Repository
+https://github.com/xotaina/Logo-Maker
+
+# Project Description
+
+A generated logo application activates the user to select a colour and shape, provide text for the logo, and save the generated SVG to a `.svg` file.
+The Application accepts user input, text or colour a hexadecimal number, and an SVG file is created, named `logo.svg`
+
+# The walkthrough video link
+
+https://drive.google.com/file/d/1B8zMiRC-vConoLNJLMaiakw0qPa1H_Z9/view
+
+# Contributor
+
+Theresa and Taina
---
+++ index.js
@@ -0,0 +1,36 @@
+const readline = require('readline')
+const {generateSvg,saveSvg} = require('./lib/shapes')
+
+const read = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+//prompt user to enter text for logo
+read.question("Enter Text for logo (Up to 3 characters): ", (text) =>{
+
+    // prompt usesr to enter text color for logo
+    read.question("Enter text color (text or hexadecimal): ", (textColor) =>{
+
+        // prompt user to select shape for logo
+        read.question("Choose shape for logo (Circle, Square Or Triangle): " , (shape) =>{
+
+            // prompt user to choose logo color
+            read.question("Enter shape color (text or hexadecimal):",(shapeColor) => {
+
+                //function to generate logo
+                const svg = generateSvg(text,textColor,shape,shapeColor)
+
+                //save SVG to file
+                saveSvg(svg, text+'.svg', (err) => {
+                    if(err){
+                        console.error(`failed to genertae SVG File: ${err}`)
+                    } else{
+                        console.log(`SVG File (${text}.svg has been generated`)
+                    }
+                    read.close();
+                })
+            });
+        });
+    });
+});
