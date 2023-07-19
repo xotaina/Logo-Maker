b840cca35bb825e45671c017803c49ecd5f7d48d
Tanisha Barreau <tanisha.barreau101@gmail.com>
2023-07-19 14:24:19 -0400

    js

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-const readline = require('readline')
-const {generateSvg,saveSvg} = require('./lib/shapes')
+const readline = require('readline');
+const { generateSvg, saveSvg } = require('./lib/shapes');
 
 const read = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
-})
-
-//prompt user to enter text for logo
-read.question("Enter Text for logo (Up to 3 characters): ", (text) =>{
-
-    // prompt usesr to enter text color for logo
-    read.question("Enter text color (text or hexadecimal): ", (textColor) =>{
-
-        // prompt user to select shape for logo
-        read.question("Choose shape for logo (Circle, Square Or Triangle): " , (shape) =>{
-
-            // prompt user to choose logo color
-            read.question("Enter shape color (text or hexadecimal):",(shapeColor) => {
-
-                //function to generate logo
-                const svg = generateSvg(text,textColor,shape,shapeColor)
-
-                //save SVG to file
-                saveSvg(svg, text+'.svg', (err) => {
-                    if(err){
-                        console.error(`failed to genertae SVG File: ${err}`)
-                    } else{
-                        console.log(`SVG File (${text}.svg has been generated`)
-                    }
-                    read.close();
-                })
-            });
-        });
-    });
+  input: process.stdin,
+  output: process.stdout
 });
+
+// Convert the question function into a promise
+const question = (query) => {
+  return new Promise(resolve => {
+    read.question(query, (answer) => {
+      resolve(answer);
+    });
+  });
+}
+
+const run = async () => {
+  try {
+    const text = await question("Enter Text for logo (Up to 3 characters): ");
+    const textColor = await question("Enter text color (text or hexadecimal): ");
+    const shape = await question("Choose shape for logo (Circle, Square Or Triangle): ");
+    const shapeColor = await question("Enter shape color (text or hexadecimal): ");
+  
+    const svg = generateSvg(text, textColor, shape, shapeColor);
+  
+    saveSvg(svg, `${text}.svg`, (err) => {
+      if(err){
+        console.error(`Failed to generate SVG File: ${err}`);
+      } else{
+        console.log(`SVG File (${text}.svg) has been generated`);
+      }
+      read.close();
+    });
+  } catch (err) {
+    console.error(`Error: ${err}`);
+    read.close();
+  }
+};
+
+run();
